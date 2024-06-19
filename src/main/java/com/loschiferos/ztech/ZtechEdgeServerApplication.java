a95640770a46bfb92fad190a8028b10ff69c3919
marzzioChicana <u202022228@upc.edu.pe>
2024-06-19 12:40:45 -0500

    first commit

---
+++ src/main/java/com/loschiferos/ztech/ZtechEdgeServerApplication.java
@@ -0,0 +1,13 @@
+package com.loschiferos.ztech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZtechEdgeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZtechEdgeServerApplication.class, args);
+	}
+
+}
